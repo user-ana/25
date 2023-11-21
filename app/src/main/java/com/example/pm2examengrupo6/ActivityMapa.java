c572bf74e2754142399dac0537eecf0ec1dbec0d
gonzbag <gonzb.hn@hotmail.com>
2023-11-21 00:29:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pm2examengrupo6/ActivityMapa.java
@@ -0,0 +1,22 @@
+package com.example.pm2examengrupo6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityMapa extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
